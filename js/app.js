7368b8b70f742b82278bc611e997fef891d27924
SoniGama <abinyuuu@gmail.com>
2018-03-07 10:42:58 -0600

    obteniendo datos de la API

--- js/app.js
+++ js/app.js
@@ -1,22 +1,52 @@
-const request = superagent;
 const getWeatherButton = document.getElementById('getWeather');
+const container = document.getElementById('container-weather');
 
-function geolocation(position) {
-    var latitude = position.coords.latitude;
-    var longitude = position.coords.longitude;
-    
-    console.log(latitude, longitude);
-    
-};
+window.addEventListener("load", () => {
+    getWeatherButton.addEventListener('click', getLocation);
+});
 
-const geolocation = () => {
-    if (!navigator.geolocation) {
-        document.getElementById('contaner-weather').innerHTML = "<p>Geolocation is not supported by your browser</p>";
-    } else {
-        getLocation();
+getLocation = (e) => {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(position);
+    } else {         
+        container.innerHTML = "Geolocation is not supported by this browser.";
     }
 }
 
-window.addEventListener("load", function () {
-    getWeatherButton.addEventListener('click', geolocation);
-});
+position = (position) => {
+    const latitude = position.coords.latitude;
+    const longitude = position.coords.longitude;
+    getJson(latitude, longitude);
+}
+
+getJson = (latitude, longitude) => {
+    fetch(`https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/4cb26c92181c81f2021169241176604f/${latitude},${longitude}/units=si`)
+    .then(response => {     
+        response.json().then( result => {
+            getDataJson(result)            
+        })        
+    })
+    .catch(error =>{
+        console.log(error);        
+    })
+}
+
+getDataJson = json => {   
+    const infoCurrently = json.currently;
+    const infoDaily = json.daily;    
+    
+    const temperature = infoCurrently.temperature;
+    const wind = infoCurrently.windSpeed;
+    const humidity = infoCurrently.humidity;
+    const uv = infoCurrently.uvIndex;
+    const pressure = infoCurrently.pressure;
+    const icon = infoCurrently.icon;
+
+    infoDaily.data.forEach(element => {         
+        const iconDay = element.icon;
+        const temperatureMax = Math.round(element.temperatureMax);
+        const temperatureMin = Math.round(element.temperatureMin);  
+        console.log();
+              
+    });   
+}
