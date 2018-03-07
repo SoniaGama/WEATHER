b8064fa5ed36e03327497ccbc99b051e3315d580
SoniGama <abinyuuu@gmail.com>
2018-03-07 12:05:04 -0600

    pintando en la tarjeta

--- js/app.js
+++ js/app.js
@@ -20,7 +20,7 @@ position = (position) => {
 }
 
 getJson = (latitude, longitude) => {
-    fetch(`https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/4cb26c92181c81f2021169241176604f/${latitude},${longitude}/units=si`)
+    fetch(`https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/4cb26c92181c81f2021169241176604f/${latitude},${longitude}/?units=ca`)
     .then(response => {     
         response.json().then( result => {
             getDataJson(result)            
@@ -31,22 +31,65 @@ getJson = (latitude, longitude) => {
     })
 }
 
+// cuando se concatenan metodos si no son anidados debe especificarse el return
+
 getDataJson = json => {   
     const infoCurrently = json.currently;
     const infoDaily = json.daily;    
     
-    const temperature = infoCurrently.temperature;
-    const wind = infoCurrently.windSpeed;
-    const humidity = infoCurrently.humidity;
-    const uv = infoCurrently.uvIndex;
-    const pressure = infoCurrently.pressure;
+    const temperature = Math.round(infoCurrently.temperature);
+    const wind = Math.round(infoCurrently.windSpeed);
+    const humidity = Math.round(infoCurrently.humidity);
+    const uv = Math.round(infoCurrently.uvIndex);
+    const pressure = Math.round(infoCurrently.pressure);
     const icon = infoCurrently.icon;
+    
+    paintCardCurrently(temperature, wind, humidity, uv, pressure, icon);
 
     infoDaily.data.forEach(element => {         
         const iconDay = element.icon;
         const temperatureMax = Math.round(element.temperatureMax);
-        const temperatureMin = Math.round(element.temperatureMin);  
-        console.log();
-              
+        const temperatureMin = Math.round(element.temperatureMin);
+        paintCardWeek(iconDay, temperatureMax, temperatureMin);           
     });   
+}
+
+paintCardCurrently = (temperature, wind, humidity, uv, pressure, icon) => {
+    const containerCurrently = document.getElementById('container-table-currently');
+     const output = `
+         <tr>
+            <td>Temperature</td>
+            <td class="data-currently">${temperature}°C</td>
+        </tr>
+        <tr>
+            <td>Wind</td>
+            <td class="data-currently">${wind}</td>
+        </tr>
+        <tr>
+            <td>Humidity</td>
+            <td class="data-currently">${humidity}</td>
+        </tr>
+        <tr>
+            <td>Uv Index</td>
+            <td class="data-currently">${uv}</td>
+        </tr>
+        <tr>
+            <td>Pressure</td>
+            <td class="data-currently">${pressure}</td>
+        </tr>   
+    `
+    containerCurrently.innerHTML = output;
+
+}
+
+paintCardWeek = (iconDay, temperatureMax, temperatureMin) => {
+    const containerWeek = document.getElementById('container-table-week');
+
+    const output = `
+        <tr>
+            <td class="data-week">${temperatureMin}°C</td>
+            <td class="data-week">${temperatureMax}°C</td>
+        </tr>                        
+    `
+    containerWeek.insertAdjacentHTML('beforeEnd', output); 
 }
